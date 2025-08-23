38b8a092473a82ebf5ec3222f8477ca131765983
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-23 18:56:52 +0900

    tenaosi

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,3 @@
-import { getRandPrimeByBitLength, getRandPrimeByRange } from '../src/main';
-
-const arr = Array(100)
-  .fill(0)
-  .map(() => getRandPrimeByBitLength(256, true));
-
-console.log(arr.join('\n'));
+import { Fraction } from '../src/main';
+const fr = Fraction.fromDecimal(Math.PI, 12);
+console.log(JSON.stringify({ fr }));
