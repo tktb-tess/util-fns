1a31950fa20d2db86e63eed55f5a232dd65fb786
Born-Bohrer <chem1000leaves@gmail.com>
2025-08-30 00:33:20 +0900

    ittan

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { bailliePSW } from '../dist/bundle';
+  
 </script>
 
 <p>
-  {bailliePSW(91n)}
+  Hello, Svelte!
 </p>
+
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'node:path';
 
 export default defineConfig({
-  plugins: [dts(), wasm(), topLevelAwait(), svelte()],
+  plugins: [dts(), wasm(), svelte()],
   build: {
     lib: {
       entry: resolve(__dirname, './lib/bundle.ts'),
