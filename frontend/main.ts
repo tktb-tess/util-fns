3bbec73a5340eb24a73c92b8f119191b3c80141d
tktb-tess <chem1000leaves@gmail.com>
2025-10-17 22:21:18 +0900

    xxx

--- frontend/main.ts
+++ frontend/main.ts
@@ -4,9 +4,23 @@ const app = document.getElementById('app')!;
 
 app.textContent = 'Press F12 to open devtools console';
 
+const obj = { ...U };
+
+Object.setPrototypeOf(obj, null);
+
 Object.defineProperty(window, '__util', {
-  value: U,
+  value: obj,
   enumerable: true,
 });
 
+const format = Intl.DateTimeFormat('ja-JP', {
+  timeZone: 'Asia/Tokyo',
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+});
 
+console.log(format.format(Date.now()));
